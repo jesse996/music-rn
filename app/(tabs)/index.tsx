4fb22e488b75000021d06522b126828431e499ca
jesse <szxfml@gmail.com>
2024-08-29 20:22:38 +0800

    更新

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,19 @@
-import { Image, StyleSheet, Platform, StatusBar, View, Text } from 'react-native';
+import { Image, StyleSheet, Platform, StatusBar, View, Text, Button } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-
+import { A } from '@expo/html-elements';
 import ImageViewer from '@/components/ImageViewer';
-import Button from '@/components/Button';
+// import Button from '@/components/Button';
 import * as ImagePicker from 'expo-image-picker';
-import { useState } from 'react';
-
+import React, { useEffect, useState } from 'react';
+import * as Linking from 'expo-linking';
 const PlaceholderImage = require('@/assets/images/background-image.png');
-
+import * as WebBrowser from 'expo-web-browser';
+import { Stack } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
 export default function HomeScreen() {
   const [selectedImage, setSelectedImage] = useState(null);
   const pickImageAsync = async () => {
@@ -27,21 +29,36 @@ export default function HomeScreen() {
       alert('You did not select any image.');
     }
   };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
+    <SafeAreaView>
+      {/* <View style={styles.imageContainer}>
         <ImageViewer
           placeholderImageSource={PlaceholderImage}
           selectedImage={selectedImage}
         />
       </View>
+      <A href="https://google.com" style={{ color: 'white', marginBottom: 10 }}>Go to Google</A>
 
       <View style={styles.footerContainer}>
         <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
         <Button label="Use this photo" />
       </View>
+      <StatusBar style="auto" /> */}
+
       <StatusBar style="auto" />
-    </View>
+
+      <Button
+        title="111Open URL with the system browser"
+        onPress={() => Linking.openURL('https://expo.dev')}
+        style={styles.button}
+      />
+      <Button
+        title="Open URL with an in-app browser"
+        onPress={() => WebBrowser.openBrowserAsync('https://expo.dev')}
+        style={styles.button}
+      />
+    </SafeAreaView>
   );
 }
 
